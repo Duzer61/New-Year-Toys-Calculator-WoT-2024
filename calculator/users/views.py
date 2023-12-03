1ceb4b17a02936e9062ac84d679afef233390fcc
Данил Кочетов <dmi194@yandex.ru>
2023-12-03 16:33:36 +0300

    Добавил шаблоны и формы для создания пользователей

--- calculator/users/views.py
+++ calculator/users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from .forms import CreationForm
+
+
+class SignUp(CreateView):
+    form_class = CreationForm
+    # Перенаправление пользователя на главную после успешной регистрации
+    # success_url = reverse_lazy('posts:index')
+    template_name = 'users/signup.html'
