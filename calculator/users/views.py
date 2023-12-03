df0e54aae13e274409d7c6008a31c60b8d1e625a
Данил Кочетов <dmi194@yandex.ru>
2023-12-03 18:30:09 +0300

    Добавил авто-логин после регистрации

--- calculator/users/views.py
+++ calculator/users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import login
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
@@ -5,7 +6,24 @@ from .forms import CreationForm
 
 
 class SignUp(CreateView):
+    """
+    Класс представления для регистрации новых пользователей.
+    """
     form_class = CreationForm
-    # Перенаправление пользователя на главную после успешной регистрации
-    # success_url = reverse_lazy('posts:index')
+    success_url = reverse_lazy('calc:index')
     template_name = 'users/signup.html'
+
+    def form_valid(self, form):
+        """
+        Переопределение метода form_valid для автоматической аутентификации
+        пользователя после успешной регистрации.
+
+        Args:
+            form (Form): Форма, содержащая данные пользователя.
+
+        Returns:
+            HttpResponse: Ответ после успешной обработки формы.
+        """
+        response = super().form_valid(form)
+        login(self.request, self.object)  # Войти в систему пользователя
+        return response
