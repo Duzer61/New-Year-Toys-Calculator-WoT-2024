c7d49ca686dd39e0bf2907893c4850b6a2658efb
Данил Кочетов <dmi194@yandex.ru>
2023-12-05 16:32:32 +0300

    Добавлено сохранение чисел в national

--- calculator/calc/urls.py
+++ calculator/calc/urls.py
@@ -6,7 +6,8 @@ app_name = 'calc'
 
 
 urlpatterns = [
-    path('', views.national, name='national'),
+    path('', views.index, name='index'),
+    path('national/', views.national, name='national'),
     path('christmas/', views.christmas, name='christmas'),
     path('eastern/', views.eastern, name='eastern'),
     path('magic/', views.magic, name='magic'),
--- calculator/calc/views.py
+++ calculator/calc/views.py
@@ -1,9 +1,41 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
+from mongodb.mongo_init import collection
+from users.models import User
 
 
+def index(request):
+    """Выводит шаблон главной страницы."""
+    if request.user.is_authenticated:
+        return redirect('calc:national')
+    return render(request, 'calc/index.html')
+
+
+@login_required
 def national(request):
     """Выводит шаблон страницы отечественной коллекции."""
+
+    username = request.user.username
+    user_data = collection.find_one({'username': username})
+
+    if request.method == 'POST':
+        crown = int(request.POST.get('crown'))
+        garland = int(request.POST.get('garland'))
+        hanging = int(request.POST.get('hanging'))
+        gift = int(request.POST.get('gift'))
+
+        new_data = {
+            'crown': crown,
+            'garland': garland,
+            'hanging': hanging,
+            'gift': gift
+        }
+
+        collection.update_one(
+            {'username': username},
+            {'$set': {'collection.national': new_data}}
+        )
+
 
 
     return render(request, 'calc/national.html')
