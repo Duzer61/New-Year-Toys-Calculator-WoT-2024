5155f60ff76146ac59b04c7a9e02f9aee29db559
Данил Кочетов <dmi194@yandex.ru>
2023-12-06 15:35:17 +0300

    Поменял порядок коллекций и ограничения

--- calculator/mongodb/initial_data.py
+++ calculator/mongodb/initial_data.py
@@ -1,28 +1,15 @@
 # Данные для заполнения коллекции игрушек при регистрации новых пользователей
 
-INITIAL_DATA = {
-    'national': {
-        'crown': 0,
-        'garland': 0,
+COLLECTION_DATA = {
         'hanging': 0,
-        'gift': 0
-    },
-    'christmas': {
         'crown': 0,
-        'garland': 0,
-        'hanging': 0,
-        'gift': 0
-    },
-    'eastern': {
-        'crown': 0,
-        'garland': 0,
-        'hanging': 0,
-        'gift': 0
-    },
-    'magic': {
-        'crown': 0,
-        'garland': 0,
-        'hanging': 0,
-        'gift': 0
+        'gift': 0,
+        'garland': 0
     }
+
+INITIAL_DATA = {
+    'national': COLLECTION_DATA,
+    'eastern': COLLECTION_DATA,
+    'magic': COLLECTION_DATA,
+    'christmas': COLLECTION_DATA,
 }
