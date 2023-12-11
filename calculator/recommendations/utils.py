d10d372a563ac6003d872e77c87c4eb4bfe5d673
Данил Кочетов <dmi194@yandex.ru>
2023-12-11 11:42:37 +0300

    Добавил все советы по крафту

--- calculator/recommendations/utils.py
+++ calculator/recommendations/utils.py
@@ -1,43 +1,13 @@
 import numpy as np
-from calc.constants import ANTIPOVTORITEL_CRAFT
+from calc.constants import (ALL_SPECIFIC_CRAFT, ANTIPOVTORITEL_CRAFT,
+                            HALF_SPECIFIC_CRAFT, UNCERT_COEF)
 
-UNCERT_COEF = 0.7  # Коэффициент неопределенности. Обозначает при какой разнице
-# значений можно рассмотреть и другой вариант
-
-# Заготовки названий для рекомендаций
-titles_translation = {
-    'national': 'Отечественная',
-    'eastern': 'Восточная',
-    'magic': 'Сказочная',
-    'christmas': 'Рождественская',
-    'hanging': 'Подвесная',
-    'crown': 'Навершие',
-    'gift': 'Подарок',
-    'garland': 'Гирлянда',
-}
-
-# Заготовки сообщений для рекомендаций
-anti_povtoritel_definitely = 'Однозначно пора использовать антиповторитель!'
-anti_povtoritel_maybe = (
-    'Чисто математически еще рано использовать антиповторитель. '
-    'Но, кажется уже лучше подстраховаться и крафтить с ним.'
-)
-congratulations = 'Все коллекции 5 лвл. собраны. Поздравляю!'
-all_random_definitely = (
-    'Выгоднее крафтить в случайной коллекции и случайной категории. '
-    'Это может занять больше попыток, но все-равно ожидаемая трата '
-    'осколков будет меньше. Не забывайте разбивать ненужные игрушки.'
-)
-all_random_maybe = (
-    'Чисто математически выгоднее крафтить в случайной коллекции и случайной '
-    'категории. Это может занять больше попыток, но все-равно ожидаемая трата '
-    'осколков меньше. Как вариант, можно воспользоваться советом ниже. '
-    'Может потратится больше осколков, но попыток будет меньше.'
-)
-collect_definitely = "Выгоднее крафтить в коллекциях: "
-collect_maybe = "Можно также крафтить в коллекциях: "
-category_definitely = "Выгоднее крафтить в категориях: "
-category_maybe = "Можно также крафтить в категориях: "
+from .message_blanks import (all_random_better_next, all_random_definitely,
+                             all_random_maybe, anti_povtoritel_definitely,
+                             anti_povtoritel_maybe, category_definitely,
+                             category_maybe, collect_category,
+                             collect_definitely, collect_maybe,
+                             congratulations, titles_translation)
 
 
 def anti_povtoritel_check(value):
@@ -53,12 +23,23 @@ def anti_povtoritel_check(value):
     return message
 
 
-def all_random_check(name, value, value_2):
+def all_random_check(name, name_2, value, value_2):
     """Проверяет на целесообразность полностью случайного крафта."""
 
     is_definitely = False  # Флаг, на однозначную целесообразность
+    # если совет может быть неоднозначный
     if name == 'all_random_min' and value_2 * UNCERT_COEF <= value:
-        message = all_random_maybe
+        # и при этом если следующий по выгодности крафт будет
+        # со 100% вероятностью
+        if name_2 == 'all_min' and value_2 == ALL_SPECIFIC_CRAFT:
+            message = all_random_better_next
+        elif (
+            (name_2 == 'category_min' or name_2 == 'collect_min')
+            and value_2 == HALF_SPECIFIC_CRAFT
+        ):
+            message = all_random_better_next
+        else:
+            message = all_random_maybe
     elif name == 'all_random_min':
         message = all_random_definitely
         is_definitely = True
@@ -68,7 +49,7 @@ def all_random_check(name, value, value_2):
     return message, is_definitely
 
 
-def collect_check(name, name_2, value, value_2, is_definitely, min_data):
+def collect_category_check(name, name_2, is_definitely, min_data):
     """Проверяет на целесообразность крафта в коллекции."""
 
     message = ''
@@ -105,6 +86,25 @@ def collect_check(name, name_2, value, value_2, is_definitely, min_data):
     return message
 
 
+def all_check(name, name_2, min_data):
+    """
+    Проверяет на целесообразность крафта в определенной коллекции
+    и определенной категории.
+    """
+    message = ''
+    if name == 'all_min' or name_2 == 'all_min':
+        message = collect_category
+        min_names = min_data['all_min']['min_names']
+        for i, titles in enumerate(min_names):
+            extra_message = (
+                f"{' - '.join(titles_translation[title]for title in titles)}"
+            )
+            message += extra_message
+            if i < len(min_names) - 1:
+                message += ', '
+    return message
+
+
 def get_advice(min_data):
     """Возвращает рекомендации по крафту."""
 
@@ -117,21 +117,22 @@ def get_advice(min_data):
         'all_min': min_data['all_min']['min_value'],
     }
     min_values = sorted(min_values.items(), key=lambda x: x[1])
-    print(f'min_values: {min_values}')
     min_name, min_value = min_values[0][0], min_values[0][1]
     min_name_2, min_value_2 = min_values[1][0], min_values[1][1]
-    min_name_3, min_value_3 = min_values[2][0], min_values[2][1]
-    rec_message = ''  # сообщение с рекомендацией крафта
     message = anti_povtoritel_check(min_value)
     message_2 = ''
     if message:  # если есть рекомендации с анитповторителем, то возвращаем их
         return message, message_2
-    message, is_definitely = all_random_check(min_name, min_value, min_value_2)
-    if message and is_definitely:
-        return message, message_2
-    message_2 = collect_check(
-        min_name, min_name_2, min_value, min_value_2, is_definitely, min_data
+    message, is_definitely = all_random_check(
+        min_name, min_name_2, min_value, min_value_2
     )
-    if not message_2:
-        message_2 = 'Пока не накодил...'
+    if message and is_definitely:  # если есть однозначная рекомендация
+        return message, message_2
+    message_2 = collect_category_check(  # дополнительный совет
+        min_name, min_name_2, is_definitely, min_data
+    )
+    if message_2:
+        return message, message_2
+
+    message_2 = all_check(min_name, min_name_2, min_data)
     return message, message_2
