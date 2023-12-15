13cb5379c637700fab7c6ab122ddcdf25e3746b6
Duzer61 <dmi194@yandex.ru>
2023-12-15 11:52:05 +0300

    Добавил формы

--- calculator/calc/forms.py
+++ calculator/calc/forms.py
@@ -1,10 +1,124 @@
 from django import forms
-
+from .models import UserAlbums
 from .constants import MAX_CROWN, MAX_GARLAND, MAX_GIFT, MAX_HANGING
 
 
-class ToysForm(forms.Form):
-    hanging = forms.IntegerField(min_value=0, max_value=MAX_HANGING)
-    crown = forms.IntegerField(min_value=0, max_value=MAX_CROWN)
-    gift = forms.IntegerField(min_value=0, max_value=MAX_GIFT)
-    garland = forms.IntegerField(min_value=0, max_value=MAX_GARLAND)
+# class ToysForm(forms.Form):
+#     hanging = forms.IntegerField(min_value=0, max_value=MAX_HANGING)
+#     crown = forms.IntegerField(min_value=0, max_value=MAX_CROWN)
+#     gift = forms.IntegerField(min_value=0, max_value=MAX_GIFT)
+#     garland = forms.IntegerField(min_value=0, max_value=MAX_GARLAND)
+helps = {
+    'hanging_text': "Количество подвесных игрушек",
+    'crown_text': "Количество наверший",
+    'gift_text': "Количество подарков",
+    'garland_text': "Количество гирлянд",
+}
+
+
+class NationalForm(forms.Form):
+    """
+    Форма для записи альбома отечественных игрушек.
+    """
+    national_hanging = forms.PositiveSmallIntegerField(
+        help_text=helps['hanging_text'],
+    )
+    national_crown = forms.PositiveSmallIntegerField(
+        help_text=helps['crown_text'],
+    )
+    national_gift = forms.PositiveSmallIntegerField(
+        help_text=helps['gift_text'],
+    )
+    national_garland = forms.PositiveSmallIntegerField(
+        help_text=helps['garland_text'],
+    )
+
+    class Meta:
+        model = UserAlbums
+        fields = [
+            'national_hanging',
+            'national_crown',
+            'national_gift',
+            'national_garland'
+        ]
+
+
+class EasternForm(forms.Form):
+    """
+    Форма для записи восточного альбома игрушек.
+    """
+    eastern_hanging = forms.PositiveSmallIntegerField(
+        help_text=helps['hanging_text'],
+    ),
+    eastern_crown = forms.PositiveSmallIntegerField(
+        help_text=helps['crown_text'],
+    ),
+    eastern_gift = forms.PositiveSmallIntegerField(
+        help_text=helps['gift_text'],
+    ),
+    eastern_garland = forms.PositiveSmallIntegerField(
+        help_text=helps['garland_text'],
+    )
+
+    class Meta:
+        model = UserAlbums
+        fields = [
+            'eastern_hanging',
+            'eastern_crown',
+            'eastern_gift',
+            'eastern_garland'
+        ]
+
+
+class MagicForm(forms.Form):
+    """
+    Форма для записи сказочного альбома игрушек.
+    """
+    magic_hanging = forms.PositiveSmallIntegerField(
+        help_text=helps['hanging_text'],
+    ),
+    magic_crown = forms.PositiveSmallIntegerField(
+        help_text=helps['crown_text'],
+    ),
+    magic_gift = forms.PositiveSmallIntegerField(
+        help_text=helps['gift_text'],
+    ),
+    magic_garland = forms.PositiveSmallIntegerField(
+        help_text=helps['garland_text'],
+    )
+
+    class Meta:
+        model = UserAlbums
+        fields = [
+            'magic_hanging',
+            'magic_crown',
+            'magic_gift',
+            'magic_garland'
+        ]
+
+
+class ChristmasForm(forms.Form):
+    """
+    Форма для записи Рождественского альбома.
+    """
+    christmas_hanging = forms.PositiveSmallIntegerField(
+        help_text=helps['hanging_text'],
+    ),
+    christmas_crown = forms.PositiveSmallIntegerField(
+        help_text=helps['crown_text'],
+    ),
+    christmas_gift = forms.PositiveSmallIntegerField(
+        help_text=helps['gift_text'],
+    ),
+    christmas_garland = forms.PositiveSmallIntegerField(
+        help_text=helps['garland_text'],
+    )
+
+    class Meta:
+        model = UserAlbums
+        fields = [
+            'christmas_hanging',
+            'christmas_crown',
+            'christmas_gift',
+            'christmas_garland'
+        ]
--- calculator/calc/models.py
+++ calculator/calc/models.py
@@ -17,67 +17,83 @@ class UserAlbums(models.Model):
     )
     national_hanging = models.PositiveSmallIntegerField(
         verbose_name='Отечественный-подвесные',
-        validators=[MaxValueValidator(MAX_HANGING)]
+        validators=[MaxValueValidator(MAX_HANGING)],
+        default=0
     )
     national_crown = models.PositiveSmallIntegerField(
         verbose_name='Отечественный-навершие',
-        validators=[MaxValueValidator(MAX_CROWN)]
+        validators=[MaxValueValidator(MAX_CROWN)],
+        default=0
     )
     national_gift = models.PositiveSmallIntegerField(
         verbose_name='Отечественный-подарки',
-        validators=[MaxValueValidator(MAX_GIFT)]
+        validators=[MaxValueValidator(MAX_GIFT)],
+        default=0
     ),
     national_garland = models.PositiveSmallIntegerField(
         verbose_name='Отечественный-гирлянды',
-        validators=[MaxValueValidator(MAX_GARLAND)]
+        validators=[MaxValueValidator(MAX_GARLAND)],
+        default=0
     ),
     eastern_hanging = models.PositiveSmallIntegerField(
         verbose_name='Восточный-подвесные',
-        validators=[MaxValueValidator(MAX_HANGING)]
+        validators=[MaxValueValidator(MAX_HANGING)],
+        default=0
     ),
     eastern_crown = models.PositiveSmallIntegerField(
         verbose_name='Восточный-навершие',
-        validators=[MaxValueValidator(MAX_CROWN)]
+        validators=[MaxValueValidator(MAX_CROWN)],
+        default=0
     ),
     eastern_gift = models.PositiveSmallIntegerField(
         verbose_name='Восточный-подарки',
-        validators=[MaxValueValidator(MAX_GIFT)]
+        validators=[MaxValueValidator(MAX_GIFT)],
+        default=0
     ),
     eastern_garland = models.PositiveSmallIntegerField(
         verbose_name='Восточный-гирлянды',
-        validators=[MaxValueValidator(MAX_GARLAND)]
+        validators=[MaxValueValidator(MAX_GARLAND)],
+        default=0
     ),
     magic_hanging = models.PositiveSmallIntegerField(
         verbose_name='Сказочный-подвесные',
-        validators=[MaxValueValidator(MAX_HANGING)]
+        validators=[MaxValueValidator(MAX_HANGING)],
+        default=0
     ),
     magic_crown = models.PositiveSmallIntegerField(
         verbose_name='Сказочный-навершие',
-        validators=[MaxValueValidator(MAX_CROWN)]
+        validators=[MaxValueValidator(MAX_CROWN)],
+        default=0
     ),
     magic_gift = models.PositiveSmallIntegerField(
         verbose_name='Сказочный-подарки',
-        validators=[MaxValueValidator(MAX_GIFT)]
+        validators=[MaxValueValidator(MAX_GIFT)],
+        default=0
     ),
     magic_garland = models.PositiveSmallIntegerField(
         verbose_name='Сказочный-гирлянды',
-        validators=[MaxValueValidator(MAX_GARLAND)]
+        validators=[MaxValueValidator(MAX_GARLAND)],
+        default=0
     ),
     christmas_hanging = models.PositiveSmallIntegerField(
         verbose_name='Рождественский-подвесные',
-        validators=[MaxValueValidator(MAX_HANGING)]
+        validators=[MaxValueValidator(MAX_HANGING)],
+        default=0
     ),
     christmas_crown = models.PositiveSmallIntegerField(
         verbose_name='Рождественский-навершие',
-        validators=[MaxValueValidator(MAX_CROWN)]
+        validators=[MaxValueValidator(MAX_CROWN)],
+        default=0
     ),
     christmas_gift = models.PositiveSmallIntegerField(
         verbose_name='Рождественский-подарки',
-        validators=[MaxValueValidator(MAX_GIFT)]
+        validators=[MaxValueValidator(MAX_GIFT)],
+        default=0
     ),
     christmas_garland = models.PositiveSmallIntegerField(
         verbose_name='Рождественский-гирлянды',
-        validators=[MaxValueValidator(MAX_GARLAND)]
+        validators=[MaxValueValidator(MAX_GARLAND)],
+        default=0
     ),
     created_at = models.DateTimeField(
         auto_now_add=True,
